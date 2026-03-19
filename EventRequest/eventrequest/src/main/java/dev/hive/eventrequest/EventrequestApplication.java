d390d92ded74eff2238388a2f49036d65b6a9e31
mjthomforde <mthomforde15@winona.edu>
2026-03-19 11:26:06 -0500

    Add files via upload

---
+++ EventRequest/eventrequest/src/main/java/dev/hive/eventrequest/EventrequestApplication.java
@@ -0,0 +1,13 @@
+package dev.hive.eventrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventrequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventrequestApplication.class, args);
+	}
+
+}
